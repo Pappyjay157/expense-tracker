06e9592a7120b0375e4c3c9edb46441d8158f6eb
Ayooluwa ogundeko <100212083+Pappyjay157@users.noreply.github.com>
2025-04-23 23:50:48 +0100

    leaflet error fix

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,14 @@ import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
+
+// Fix Leaflet's default icon paths
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon2x.src,
-  iconUrl: markerIcon.src,
-  shadowUrl: markerShadow.src,
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
 });
 
 function App() {
